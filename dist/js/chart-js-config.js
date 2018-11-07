de6a51dcee39258052e67066b6aa7357431f7666
Alexander Rechsteiner <alex@wildmind.ch>
2018-11-07 19:12:34 +0100

    Notification and chart

---
+++ dist/js/chart-js-config.js
@@ -0,0 +1,14 @@
+/*!
+ * Spur - An admin template based on Bootstrap 4
+ * Version v0.1.0
+ * Copyright 2016 - 2018 Alexander Rechsteiner
+ * https://hackerthemes.com
+ */
+
+Chart.defaults.global.defaultFontFamily = "'Open Sans', 'sans-serif'";
+
+window.chartColors = {
+    red: '#dc3545',
+    blue: '#007bff',
+    green: '#28a745'
+};
---
+++ js/chart-js-config.js
@@ -0,0 +1,7 @@
+Chart.defaults.global.defaultFontFamily = "'Open Sans', 'sans-serif'";
+
+window.chartColors = {
+    red: '#dc3545',
+    blue: '#007bff',
+    green: '#28a745'
+};
