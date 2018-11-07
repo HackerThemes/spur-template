22d87924fe2d889bb7b278e8595601df0e053b78
Alexander Rechsteiner <alex@wildmind.ch>
2018-11-07 21:33:50 +0100

    Chart examples:

--- js/chart-js-config.js
+++ js/chart-js-config.js
@@ -1,7 +1,11 @@
 Chart.defaults.global.defaultFontFamily = "'Open Sans', 'sans-serif'";
 
 window.chartColors = {
-    red: '#dc3545',
-    blue: '#007bff',
-    green: '#28a745'
+    primary: '#007bff',
+    secondary: '#6610f2',
+    success: '#28a745',
+    info: '#17a2b8',
+    warning: '#ffc107',
+    superwarning: '#fd7e14',
+    danger: '#dc3545',
 };
