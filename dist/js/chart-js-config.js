e2802bb1df581b60910d56eaffa4b0b613740109
Alexander Rechsteiner <alex@wildmind.ch>
2019-01-18 17:24:55 +0100

    Theming and some bugfixes before v1

--- dist/js/chart-js-config.js
+++ dist/js/chart-js-config.js
@@ -8,11 +8,11 @@
 Chart.defaults.global.defaultFontFamily = "'Open Sans', 'sans-serif'";
 
 window.chartColors = {
-    primary: '#007bff',
-    secondary: '#6610f2',
-    success: '#28a745',
-    info: '#17a2b8',
-    warning: '#ffc107',
+    primary: '#3F84FC',
+    secondary: '#727F94',
+    success: '#1DAB47',
+    info: '#8A3FFC',
+    warning: '#FCAE3F',
     superwarning: '#fd7e14',
-    danger: '#dc3545',
+    danger: '#FC413F',
 };
